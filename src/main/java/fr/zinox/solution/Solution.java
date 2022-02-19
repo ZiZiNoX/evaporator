30281ba6e5c4fb89abb7b8c8feaff04cf5cd6830
ZiNoX <emilio.bizouarn@gmail.com>
2022-02-19 16:29:23 +0100

    Removing empty spaces between lines

--- src/main/java/fr/zinox/solution/Solution.java
+++ src/main/java/fr/zinox/solution/Solution.java
@@ -7,16 +7,14 @@ import java.util.concurrent.ThreadLocalRandom;
  * @author ZiNoX
  */
 
-public class Solution
-{
+public class Solution {
     private static final int ROWS_MIN = 3;
     private static final int ROWS_MAX = 50;
 
     private static final char EVAPORATOR_CASE_CHAR = 'x';
     private static final char EMPTY_CASE_CHAR = '.';
 
-    public static void main(String[] args) throws InterruptedException
-    {
+    public static void main(String[] args) throws InterruptedException {
         final Scanner scanner = new Scanner(System.in);
 
         int lines;
@@ -33,15 +31,12 @@ public class Solution
 
         randomRows = scanner.nextLine().equalsIgnoreCase("yes");
 
-        if (randomNumber)
-        {
+        if (randomNumber) {
             lines = ThreadLocalRandom.current().nextInt(ROWS_MIN, ROWS_MAX);
 
             System.out.println("The random number is: " + lines);
-        } else
-        {
-            do
-            {
+        } else {
+            do {
                 System.out.println("You have chosen to enter your own number.");
                 System.out.println("Please enter a number between " + ROWS_MIN + " and " + ROWS_MAX + " representing your li number");
                 lines = scanner.nextInt();
@@ -51,20 +46,16 @@ public class Solution
         final String[] patterns = new String[lines];
         final char[][] farm = new char[lines][lines];
 
-        if (randomRows)
-        {
-            for (int i = 0; i < lines; i++)
-            {
+        if (randomRows) {
+            for (int i = 0; i < lines; i++) {
                 final StringBuilder pattern = new StringBuilder();
 
-                for (int j = 0; j < lines; j++)
-                {
+                for (int j = 0; j < lines; j++) {
                     pattern.append(ThreadLocalRandom.current().nextBoolean() ? EVAPORATOR_CASE_CHAR : EMPTY_CASE_CHAR);
                 }
                 patterns[i] = pattern.toString();
             }
-        } else
-        {
+        } else {
             System.out.println("Sorry but the only farm random generation feature is developed...");
             System.out.println("So you can't get in your own farm... Sorry");
             Thread.sleep(5000);
@@ -72,18 +63,14 @@ public class Solution
         }
 
         System.out.println("This is what the grid looks like");
-        for (int i = 0; i <= farm.length - 1; i++)
-        {
+        for (int i = 0; i <= farm.length - 1; i++) {
             final StringBuilder stringBuilder = new StringBuilder();
-            for (int j = 0; j <= farm[i].length - 1; j++)
-            {
+            for (int j = 0; j <= farm[i].length - 1; j++) {
                 final char currentChar = patterns[i].charAt(j);
                 farm[i][j] = currentChar;
-                if (currentChar == EMPTY_CASE_CHAR)
-                {
+                if (currentChar == EMPTY_CASE_CHAR) {
                     cultivableHuts++;
                 }
-
                 stringBuilder.append(currentChar);
             }
             System.out.println(stringBuilder);
